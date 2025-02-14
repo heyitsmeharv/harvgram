09f3e5f3af25fd7046cceba5d0a840ac0fa5b544
Adam Harvey <heyitsmeharv@gmail.com>
2025-02-14 19:24:01 +0000

    material ui

--- infrastructure/environments/sandbox/providers.tf
+++ infrastructure/environments/sandbox/providers.tf
@@ -1,3 +1,17 @@
+provider "aws" {
+  profile = "assume-role-sandbox"
+  default_tags {
+    tags = {
+      environment = "sandbox"
+      project     = "harvgram"
+      provisioner = "terraform"
+    }
+  }
+
+  region = "eu-west-2"
+  alias  = "eu-west-2"
+}
+
 terraform {
   required_version = ">= 1.7.3"
   required_providers {
@@ -12,26 +26,5 @@ terraform {
     key     = "sandbox/terraform.tfstate"
     region  = "eu-west-2"
     encrypt = true
-
-    assume_role = {
-      role_arn = "arn:aws:iam::039612873345:role/AssumeSandboxAccount"
-    }
   }
-}
-
-provider "aws" {
-  assume_role {
-    role_arn = var.role_arn
-  }
-
-  default_tags {
-    tags = {
-      environment = "sandbox"
-      project     = "harvgram"
-      provisioner = "terraform"
-    }
-  }
-
-  region = "eu-west-2"
-  alias  = "eu-west-2"
 }
