dad3d1422e3f3068d8a2d710fcd2d51671582ace
Adam Harvey <heyitsmeharv@gmail.com>
2025-03-03 21:57:22 +0000

    dialog added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Button, Container, Card, CardMedia, Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Button, Container, Card, CardMedia, Box, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./styles/themes/theme.jsx";
 import './App.css';
@@ -10,6 +10,7 @@ import { Header } from "./components/Header/Header.jsx";
 import { Photo } from "./components/Photo/Photo.jsx";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [images, setImages] = useState([{ id: 1, date: '20/02/2025', pictureUrl: image, title: "test", tag: "test", caption: "test" }]);
 
   useEffect(() => {
@@ -32,11 +33,24 @@ function App() {
       <Box sx={{ display: "flex", flexDirection: "column", height: "100vh - 64px" }}>
         <Header onUpload={handleUpload} onLogin={handleLogin} />
         <Box sx={{ flexGrow: 1, marginTop: "64px", padding: 2, backgroundColor: "primary.accent" }}>
-          <Container sx={{ flexGrow: 1 }}>
-            {images.map(img => {
-              return  <Photo key={img.id} date={img.createdAt} image={img.pictureUrl} title={img.title} tag={img.tag} caption={img.caption} />
-            })}
-          </Container>
+          {isLoggedIn ?
+            <Container sx={{ flexGrow: 1 }}>
+              {images.map(img => {
+                return <Photo key={img.id} date={img.createdAt} image={img.pictureUrl} title={img.title} tag={img.tag} caption={img.caption} />
+              })}
+            </Container>
+            : <Dialog open={!isLoggedIn} onClose={() => setIsLoggedIn(true)}>
+              <DialogTitle>Welcome!</DialogTitle>
+              <DialogContent>
+                <p>Please log in or sign up to continue.</p>
+              </DialogContent>
+              <DialogActions>
+                <Button variant="contained" color="primary">Login</Button>
+                <Button variant="contained" color="secondary">Request Access</Button>
+                <Button onClick={() => setIsLoggedIn(true)} color="inherit">Close</Button>
+              </DialogActions>
+            </Dialog>
+          }
         </Box>
       </Box>
     </ThemeProvider>
