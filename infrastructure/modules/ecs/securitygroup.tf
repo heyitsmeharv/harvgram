bef6ba60bdb1ab3b77f14973cf9c6709fc0aeefe
Adam Harvey <heyitsmeharv@gmail.com>
2025-05-14 20:11:34 +0100

    fix circular dependancy

--- infrastructure/modules/ecs/securitygroup.tf
+++ infrastructure/modules/ecs/securitygroup.tf
@@ -1,47 +1,64 @@
+resource "aws_security_group" "frontend_sg" {
+  name        = "harvgram-frontend-sg"
+  description = "Frontend ECS SG"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "frontend-sg"
+  }
+}
+
 resource "aws_security_group" "backend_sg" {
   name        = "harvgram-backend-sg"
-  description = "Allow traffic only from frontend ECS"
-  vpc_id      = var.vpc_id
-
-   ingress {
-    from_port       = 5002
-    to_port         = 5002
-    protocol        = "tcp"
-    security_groups = [aws_security_group.frontend_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  description = "Backend ECS SG"
+  vpc_id      = aws_vpc.main.id
 
   tags = {
     Name = "backend-sg"
   }
 }
 
-resource "aws_security_group" "frontend_sg" {
-  name        = "harvgram-frontend-sg"
-  description = "Allow outbound to backend and inbound from ALB"
-  vpc_id      = var.vpc_id
+resource "aws_security_group_rule" "alb_to_frontend" {
+  type                     = "ingress"
+  from_port                = 3000
+  to_port                  = 3000
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.frontend_sg.id
+  source_security_group_id = var.alb_sg_id
+}
 
-  ingress {
-    from_port       = 3000
-    to_port         = 3000
-    protocol        = "tcp"
-    security_groups = [var.alb_sg_id]
-  }
+resource "aws_security_group_rule" "frontend_to_backend" {
+  type                     = "egress"
+  from_port                = 5002
+  to_port                  = 5002
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.frontend_sg.id
+  source_security_group_id = aws_security_group.backend_sg.id
+}
 
-  egress {
-    from_port       = 5002
-    to_port         = 5002
-    protocol        = "tcp"
-    security_groups = [aws_security_group.backend_sg.id]
-  }
+resource "aws_security_group_rule" "backend_from_frontend" {
+  type                     = "ingress"
+  from_port                = 5002
+  to_port                  = 5002
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.backend_sg.id
+  source_security_group_id = aws_security_group.frontend_sg.id
+}
 
-  tags = {
-    Name = "frontend-sg"
-  }
+resource "aws_security_group_rule" "backend_to_frontend" {
+  type                     = "egress"
+  from_port                = 3000
+  to_port                  = 3000
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.backend_sg.id
+  source_security_group_id = aws_security_group.frontend_sg.id
+}
+
+resource "aws_security_group_rule" "frontend_from_backend" {
+  type                     = "ingress"
+  from_port                = 3000
+  to_port                  = 3000
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.frontend_sg.id
+  source_security_group_id = aws_security_group.backend_sg.id
 }
