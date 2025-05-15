19af90a8a4c0b2be11f7d88751af7f4b18d90c11
Adam Harvey <heyitsmeharv@gmail.com>
2025-05-15 19:35:12 +0100

    missed variable

--- infrastructure/modules/network/variables.tf
+++ infrastructure/modules/network/variables.tf
@@ -1,3 +1,8 @@
+variable "private_subnet_ids" {
+  description = "list of private subnet ids"
+  type        = list(string)
+}
+
 variable "security_groups" {
   description = "frontend and backend security groups"
   type        = list(string)
