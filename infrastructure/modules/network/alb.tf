fa7927f2b80f5db50ba6b8cca77e8a0bff2d824b
Adam Harvey <heyitsmeharv@gmail.com>
2025-05-16 16:43:05 +0100

    alb to https

--- infrastructure/modules/network/alb.tf
+++ infrastructure/modules/network/alb.tf
@@ -75,9 +75,9 @@ resource "aws_security_group" "alb_sg" {
   description = "Allow HTTP inbound traffic"
   vpc_id      = aws_vpc.main.id
 
-   ingress {
-    from_port   = 80
-    to_port     = 80
+  ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
