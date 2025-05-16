70f4f8bab03698be3927ef961ce4681b836a92c7
Adam Harvey <heyitsmeharv@gmail.com>
2025-05-16 16:30:56 +0100

    arn

--- infrastructure/modules/network/alb.tf
+++ infrastructure/modules/network/alb.tf
@@ -62,7 +62,7 @@ resource "aws_lb_listener" "https" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = data.aws_acm_certificate.auth_cert_london
+  certificate_arn   = data.aws_acm_certificate.auth_cert_london.arn
 
   default_action {
     type             = "forward"
