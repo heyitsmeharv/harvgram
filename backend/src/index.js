170d8737e99d56320edadc0963ea87af62c6bb1a
Adam Harvey <heyitsmeharv@gmail.com>
2025-05-17 13:27:04 +0100

    docker changes
    docker changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,8 +9,6 @@ import pictureRoutes from "./routes/pictureRoutes.js";
 
 const app = express();
 
-app.set('trust proxy', true);
-
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
