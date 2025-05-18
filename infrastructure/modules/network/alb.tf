b36cc32eb5bbfe2da0407bac671bd1375764c563
Adam Harvey <heyitsmeharv@gmail.com>
2025-05-18 13:28:09 +0100

    default health check

--- infrastructure/modules/network/alb.tf
+++ infrastructure/modules/network/alb.tf
@@ -23,7 +23,7 @@ resource "aws_lb_target_group" "frontend" {
   target_type = "ip"
 
   health_check {
-    path                = "/health.html"
+    path                = "/"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
