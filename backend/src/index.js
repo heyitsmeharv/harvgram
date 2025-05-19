c213cf91826c391c12bd1dd27a81a45e54e6d24d
Adam Harvey <heyitsmeharv@gmail.com>
2025-05-19 11:16:43 +0100

    5173 -> 3000

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: ["http://localhost:5173", "https://www.harvgram.co.uk"], methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], allowedHeaders: ["Content-Type", "Authorization"] }));
+app.use(cors({ origin: ["http://localhost:3000", "https://www.harvgram.co.uk"], methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], allowedHeaders: ["Content-Type", "Authorization"] }));
 
 app.use("/api", healthRoute);
 app.use("/api", authRoutes);
