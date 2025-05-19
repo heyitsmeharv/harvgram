0a05bd2957dd6824abab2d58ea0086fe998350c1
Adam Harvey <heyitsmeharv@gmail.com>
2025-05-19 19:31:26 +0100

    output full image

--- infrastructure/environments/sandbox/outputs.tf
+++ infrastructure/environments/sandbox/outputs.tf
@@ -5,3 +5,11 @@ output "frontend_image_debug" {
 output "backend_image_debug" {
   value = var.backend_image_tag
 }
+
+output "front_end_image_uri_debug" {
+  value = "${module.ecr.frontend_image_url}:${var.frontend_image_tag}"
+}
+
+output "back_end_image_uri_debug" {
+  value = "${module.ecr.backend_image_url}:${var.backend_image_tag}"
+}
