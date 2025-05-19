e6cbcadf89e16340af2ca4727cf50e0f4d6811ab
Adam Harvey <heyitsmeharv@gmail.com>
2025-05-19 11:01:28 +0100

    move container def to local

--- infrastructure/modules/ecs/locals.tf
+++ infrastructure/modules/ecs/locals.tf
@@ -2,3 +2,53 @@ locals {
   branch = replace(basename(path.cwd), "_", "-")
   name   = "harvgram"
 }
+
+locals {
+  frontend_container_definition = [
+    {
+      name      = "frontend"
+      image     = "${var.frontend_image_url}:${var.frontend_image_tag}"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-region        = "eu-west-2"
+          awslogs-group         = aws_cloudwatch_log_group.ecs_frontend.name
+          awslogs-stream-prefix = "frontend"
+        }
+      }
+    }
+  ]
+}
+
+locals {
+  backend_container_definition = [
+    {
+      name      = "backend"
+      image     = "${var.backend_image_url}:${var.backend_image_tag}"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 5002
+          hostPort      = 5002
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-region        = "eu-west-2"
+          awslogs-group         = aws_cloudwatch_log_group.ecs_backend.name
+          awslogs-stream-prefix = "backend"
+        }
+      }
+    }
+  ]
+}
--- infrastructure/modules/ecs/taskdefinition.tf
+++ infrastructure/modules/ecs/taskdefinition.tf
@@ -25,29 +25,7 @@ resource "aws_ecs_task_definition" "frontend" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_role.arn
-
-  container_definitions = jsonencode([
-    {
-      name      = "frontend"
-      image     = "${var.frontend_image_url}:${var.frontend_image_tag}"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-          protocol      = "tcp"
-        }
-      ],
-      log_configuration = {
-        log_driver = "awslogs"
-        options = {
-          awslogs-region        = "eu-west-2"
-          awslogs-group         = aws_cloudwatch_log_group.ecs_frontend.name
-          awslogs-stream-prefix = "frontend"
-        }
-      }
-    }
-  ])
+  container_definitions    = jsonencode(local.frontend_container_definition)
 }
 
 resource "aws_ecs_task_definition" "backend" {
@@ -57,27 +35,5 @@ resource "aws_ecs_task_definition" "backend" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_role.arn
-
-  container_definitions = jsonencode([
-    {
-      name      = "backend"
-      image     = "${var.backend_image_url}:${var.backend_image_tag}"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 5002
-          hostPort      = 5002
-          protocol      = "tcp"
-        }
-      ],
-      log_configuration = {
-        log_driver = "awslogs"
-        options = {
-          awslogs-region        = "eu-west-2"
-          awslogs-group         = aws_cloudwatch_log_group.ecs_backend.name
-          awslogs-stream-prefix = "backend"
-        }
-      }
-    }
-  ])
+  container_definitions    = jsonencode(local.backend_container_definition)
 }
