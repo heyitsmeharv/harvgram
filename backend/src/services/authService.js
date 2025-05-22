da9c617b10b9a6fb12ffd60c01ae8c2d2f53692e
Adam Harvey <heyitsmeharv@gmail.com>
2025-05-22 16:57:38 +0100

    parse

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -11,11 +11,25 @@ dotenv.config({ path: process.env.NODE_ENV === "production" ? ".env.production" 
 
 const client = new CognitoIdentityProviderClient({ region: process.env.AWS_REGION });
 
+const getSecretValue = secret => {
+  try {
+    const obj = JSON.parse(secret);
+    return Object.values(obj)[0];
+  } catch {
+    return secret;
+  }
+}
+
+const USER_POOL_ID = getSecretValue(process.env.COGNITO_USER_POOL_ID);
+const CLIENT_ID    = getSecretValue(process.env.COGNITO_CLIENT_ID);
+
+console.log("details", USER_POOL_ID, CLIENT_ID)
+
 export const initiateLogin = async (email, password) => {
   const command = new AdminInitiateAuthCommand({
     AuthFlow: "ADMIN_NO_SRP_AUTH",
-    UserPoolId: process.env.COGNITO_USER_POOL_ID,
-    ClientId: process.env.COGNITO_CLIENT_ID,
+    UserPoolId: COGNITO_USER_POOL_ID,
+    ClientId: COGNITO_CLIENT_ID,
     AuthParameters: {
       USERNAME: email,
       PASSWORD: password,
@@ -36,8 +50,8 @@ export const initiateLogin = async (email, password) => {
 export const completeChangePassword = async (session, email, newPassword) => {
   const command = new AdminRespondToAuthChallengeCommand({
     ChallengeName: "NEW_PASSWORD_REQUIRED",
-    ClientId: process.env.COGNITO_CLIENT_ID,
-    UserPoolId: process.env.COGNITO_USER_POOL_ID,
+    ClientId: COGNITO_CLIENT_ID,
+    UserPoolId: COGNITO_USER_POOL_ID,
     Session: session,
     ChallengeResponses: {
       USERNAME: email,
